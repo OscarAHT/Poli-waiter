149bee1a9ea612cc6b39aeca516b417a62646bb6
CarlosMatiaSSJ <carlosmatias@MacBook-Air-de-Carlos-4.local>
2023-01-31 14:31:59 -0600

    .

--- resources/views/Alimentos/registrarComida.blade.php
+++ resources/views/Alimentos/registrarComida.blade.php
@@ -4,15 +4,27 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Nuevo Alimento</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          ...
+            <div class="form">
+                <select class="form-select" aria-label="Default select example">
+                    <option selected disabled>Tipo de alimento: </option>
+                    <option value="1">Comida</option>
+                    <option value="2">Bebida</option>
+                    <option value="3">Snack</option>
+                  </select>
+                  <div class="input-group mt-2">
+                    <span class="input-group-text">Nombre:</span>
+                    <textarea class="form-control" aria-label="With textarea"></textarea>
+                  </div>
+                  
+            </div>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+          <button type="button" class="btn btn-primary">Agregar</button>
         </div>
       </div>
     </div>
