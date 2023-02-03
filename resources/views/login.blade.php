0740ae06e693a4c27bef8bc266efe385727fcb72
CarlosMatiaSSJ <carlosmatias@MacBook-Air-de-Carlos-4.local>
2023-02-02 22:33:38 -0600

    vista login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+    <link rel="stylesheet" href="{{asset('css/styleLogin.css')}}">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -10,12 +11,23 @@
 
 <body>
     {{-- Formulario inicio sesión  --}}
-    <div class="container">
-        <form action="">
-
+    
+    <div class="login-box">
+        <img src="{{asset('imgs/icon.jpeg')}}" class="avatar" alt="Avatar Image">
+        <h1>Poli-Waiter | Login </h1>
+        <form>
+          <!-- USERNAME INPUT -->
+          <label for="username">Usuario: </label>
+          <input type="text" placeholder="">
+          <!-- PASSWORD INPUT -->
+          <label for="password">Contraseña: </label>
+          <input type="password" placeholder="">
+          <input type="submit" value="Ingresar">
+          <a href="#">¿Olvidaste tu contraseña?</a>
+         
         </form>
-    </div>
+      </div>
 
 </body>
 
-</html>ml
+</html>
